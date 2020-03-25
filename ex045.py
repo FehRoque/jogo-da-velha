1b96ae66fde0da77d413bd911481f2e8ead587c7
Felipe Roque <feliperamosroque@gmail.com>
2020-03-25 19:37:50 -0300

    Atualizado

--- ex045.py
+++ ex045.py
@@ -1,21 +1,30 @@
 from random import randint
 from time import sleep
+
 itens = ('Pedra', 'Papel', 'Tesoura')
 computador = randint(0, 2)
+
 print('''Suas opções :
 [0] PEDRA
 [1] PAPEL
 [2] TESOURA''')
+
 jogador = int(input('Qual a sua jogada? '))
+
 print('JO')
 sleep(1)
+
 print('KEN')
 sleep(1)
+
 print('PÔ!!!')
 print('-='*12)
+
 print('Jogador jogou {}'.format(itens[jogador]))
 print('Computador jogou {}'.format(itens[computador]))
+
 print('='*12)
+
 if computador == 0: ##Computador jogou PEDRA
     if jogador == 0:
         print('EMPATE! ')
@@ -25,6 +34,7 @@ if computador == 0: ##Computador jogou PEDRA
         print('COMPUTADOR VENCEU ')
     else:
         print('JOGADA INVÁLIDA! ')
+
 elif computador == 1:  ##Computador jogou PAPEL
     if jogador == 0:
         print('COMPUTADOR VENCEU ')
@@ -34,6 +44,7 @@ elif computador == 1:  ##Computador jogou PAPEL
         print('JOGADOR VENCEU ')
     else:
         print('JOGADA INVÁLIDA! ')
+
 elif computador == 2:  #Computador jogou TESOURA
     if jogador == 0:
         print('JOGADOR VENCEU ')
